f60463ce8dc32f9128c852ba71d5a865f3eb8356
vidyaqph <vidya.giree@yahoo.com>
2018-10-28 22:27:10 -0400

    dd

--- src/app/employees/display-employee.component.ts
+++ src/app/employees/display-employee.component.ts
@@ -1,5 +1,5 @@
 // This is a child component of listEmployee component
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges,  SimpleChanges } from '@angular/core';
 import { Employee } from '../models/employee.model';
 
 @Component({
@@ -7,11 +7,32 @@ import { Employee } from '../models/employee.model';
   templateUrl: './display-employee.component.html',
   styleUrls: ['./display-employee.component.css']
 })
-export class DisplayEmployeeComponent implements OnInit {
- @Input() emp: Employee; // Data comes from parent component listEmployee
+export class DisplayEmployeeComponent implements OnInit, OnChanges { // OnChanges is to capture previous and next emp data
+// @Input() emp: Employee; // Data comes from parent component listEmployee, declared to capture input chnages using ngOnChnges
+ @Input() employeeId: number;
+ private _employee: Employee; // _indicates private variable
+ @Input()
+ set emp(value: Employee) {
+   console.log('Setter Previous - '+ this._employee?this._employee.name:"Null");
+   console.log('Setter Current - ' + value.name);
+
+   this._employee = value;
+ }
+get emp(): Employee {
+  return this._employee;
+}
   constructor() { }
 
   ngOnInit() {
+  }
+  // Tracks the input chnages using ngOnChanges
+  ngOnChanges(changes: SimpleChanges) {
+    // const previousEmp =  <Employee>changes.emp.previousValue ;
+    // const currentEmp = <Employee>changes.emp.currentValue;
+    // console.log('Previous : ' + (previousEmp ? previousEmp.name: 'Null');
+    // console.log('Current :' + currentEmp.name);
+
+      // console.log(changes);
   }
 
 }
